ae69ad374b0a113ab9c5d62c21917b9ae98622f4
aleks <aleks@email.noreply.com>
2024-01-09 00:02:41 +0500

    [Feature] Add EmailIncorrectException.cs.

---
+++ MinApiEssential/Users/EmailIncorrectException.cs
@@ -0,0 +1,6 @@
+﻿namespace MinApiEssential.Users;
+
+internal class EmailIncorrectException(string mail, string? message = null) : Exception(message)
+{
+    public string Mail { get; } = mail;
+}
--- MinApiEssential/Users/UserRequest.cs
+++ MinApiEssential/Users/UserRequest.cs
@@ -1,3 +1,38 @@
 ﻿namespace MinApiEssential.Users;
 
-public record UserRequest(string Name, string Email);
+using System.Net.Mail;
+public record UserRequest
+{
+    private readonly string _email = null!;
+    /// <summary>
+    ///     Application User.
+    /// </summary>
+    /// <param name="Name">Name of the user.</param>
+    /// <param name="Email">Email of the user.</param>
+    public UserRequest(string Name, string Email)
+    {
+        this.Name = Name;
+        this.Email = Email;
+    }
+
+    /// Name of the user.
+    public string Name { get; }
+    
+    /// Email of the user.
+    public string Email
+    {
+        get
+        {
+            return _email;
+        }
+        private init
+        {
+            if (!MailAddress.TryCreate(value, out MailAddress? address))
+            {
+                throw new EmailIncorrectException(value);
+            }
+
+            _email = address.Address;
+        }
+    }
+}
