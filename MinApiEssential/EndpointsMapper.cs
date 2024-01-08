a880a7dbf34a06cdf1a4608a343c3b1f62f30c46
aleks <aleks@email.noreply.com>
2024-01-08 19:57:17 +0500

    [Feature] Apply local functions for endpoints structure.

--- MinApiEssential/EndpointsMapper.cs
+++ MinApiEssential/EndpointsMapper.cs
@@ -1,12 +1,13 @@
 ﻿#pragma warning disable CS1591// Missing XML comment for publicly visible type or member
-using Microsoft.AspNetCore.Mvc;
-using MinApiEssential.Extensions;
-using MinApiEssential.Filters;
-using MinApiEssential.Models;
-using System.Net.Mime;
-
 namespace MinApiEssential;
 
+using Microsoft.AspNetCore.Mvc;
+using Extensions;
+using Models;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Collections.Immutable;
+using System.Net.Mail;
 public static class EndpointsMapper
 {
     public static void MapMinimalEndpoints(this WebApplication app)
@@ -20,7 +21,7 @@ public static class EndpointsMapper
         RouteGroupBuilder group = app.MapGroup("/test")
             .WithTags("Test endpoints group");
 
-        group.MapGet("/greet", () => "hello!")
+        group.MapGet("/greet", GetGreet)
             .WithSummary("Test API endpoint")
             .WithDescription("Test endpoint")
             .Produces<string>(
@@ -32,6 +33,12 @@ public static class EndpointsMapper
                 contentType: MediaTypeNames.Text.Plain,
                 description: "Server is not responding",
                 example: "Internal server error, try again later");
+        return;
+
+        string GetGreet()
+        {
+            return "hello!";
+        }
     }
 
     private static void MapUserEndpoints(IEndpointRouteBuilder app)
@@ -39,23 +46,48 @@ public static class EndpointsMapper
         RouteGroupBuilder userGroup = app.MapGroup("/user")
             .WithTags("Users API");
 
-        userGroup.MapGet("/{id:guid}", (Guid id) => new User(id, "Alex", "alex@m.ru"))
+        userGroup.MapGet("/{id:guid}", GetUser)
             .WithSummary("Find a user by id")
             .Produces<User>(description: "Found User");
 
-        userGroup.MapPost("/", (
-                [FromBody] UserRequest userRequest,
-                ILogger<Program> logger) =>
-            {
-                User created = new(Guid.NewGuid(), userRequest.Name, userRequest.Email);
-                logger.LogInformation("Created user: {Created}", created);
-                return created.Id.ToString();
-            })
+        userGroup.MapPost("/", PostUser)
             .WithSummary("Create User")
-            .Produces<string>(description: "ID of the created user", example: Guid.NewGuid().ToString())
+            .Produces<Guid>(description: "ID of the created user")
             .Produces<ProblemDetails>(
                 statusCode: StatusCodes.Status422UnprocessableEntity,
-                description: "If the request body model is invalid")
-            .AddEndpointFilter<UserRequestValidationFilter>();
+                description: "If the request body model is invalid");
+        return;
+
+        IResult PostUser([FromBody] UserRequest userRequest,
+                         [FromServices] ILogger<Program> logger)
+        {
+            if (ValidateUserRequest(userRequest) is ProblemHttpResult problem)
+            {
+                logger.LogInformation("Generated problem {@Problem}", problem);
+                return problem;
+            }
+
+            User created = new(Guid.NewGuid(), userRequest.Name, userRequest.Email);
+            logger.LogInformation("Created user: {Created}", created);
+
+            return Results.Ok(created.Id);
+        }
+
+        User GetUser([FromRoute] Guid id)
+        {
+            return new User(id, "Alex", "alex@m.ru");
+        }
+
+        IResult ValidateUserRequest(UserRequest userRequest)
+        {
+            if (!MailAddress.TryCreate(userRequest.Email, out _))
+            {
+                return Results.ValidationProblem(
+                    errors: ImmutableDictionary<string, string[]>.Empty.Add("Email", [ "Email is invalid" ]),
+                    statusCode: StatusCodes.Status422UnprocessableEntity);
+            }
+
+            return Results.Ok();
+        }
     }
 }
