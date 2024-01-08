a125c679ab85955890bef131f4272e408426cd8e
aleks <aleks@email.noreply.com>
2024-01-09 00:02:17 +0500

    [feature] Add UserResponse.cs instead os User.

---
+++ MinApiEssential/Users/UserResponse.cs
@@ -0,0 +1,46 @@
+﻿namespace MinApiEssential.Users;
+
+using System.Net.Mail;
+/// <summary>
+///     Application User.
+/// </summary>
+public record UserResponse
+{
+    private readonly string _email = null!;
+    /// <summary>
+    ///     Application User.
+    /// </summary>
+    /// <param name="Id">ID of the user.</param>
+    /// <param name="Name">Name of the user.</param>
+    /// <param name="Email">Email of the user.</param>
+    public UserResponse(Guid Id, string Name, string Email)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Email = Email;
+    }
+    
+    /// Email of the user.
+    public string Email
+    {
+        get
+        {
+            return _email;
+        }
+        private init
+        {
+            if (!MailAddress.TryCreate(value, out MailAddress? address))
+            {
+                throw new EmailIncorrectException(value);
+            }
+
+            _email = address.Address;
+        }
+    }
+
+    /// ID of the user.
+    public Guid Id { get; }
+
+    /// Name of the user.
+    public string Name { get; }
+}
